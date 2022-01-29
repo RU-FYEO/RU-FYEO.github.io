07f95b49f7b7cab8b25bea2b97bef45c569559c7
RU-FYEO <93728272+RU-FYEO@users.noreply.github.com>
2022-01-29 17:59:25 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -39,18 +39,18 @@
     // a boost of 10 to indicate matches on this field are more important.
     var idx = lunr(function () {
       this.field('id');
-      this.field('title', { boost: 10 });
-      this.field('author');
-      this.field('category');
+      this.field('Question', { boost: 10 });
+      this.field('Answer');
+      this.field('Category');
       this.field('content');
     });
 
     for (var key in window.store) { // Add the data to lunr
       idx.add({
         'id': key,
-        'title': window.store[key].title,
-        'author': window.store[key].author,
-        'category': window.store[key].category,
+        'Question': window.store[key].title,
+        'Answer': window.store[key].author,
+        'Category': window.store[key].category,
         'content': window.store[key].content
       });
 
